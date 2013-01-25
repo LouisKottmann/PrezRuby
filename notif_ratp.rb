66b4474d5073c372d94e0db8231e378a7cf8c5d9
Louis Kottmann <louis.kottmann@gmail.com>
2013-01-25 14:08:51 +0100

    renamed variable in ratp script

--- notif_ratp.rb
+++ notif_ratp.rb
@@ -14,8 +14,8 @@ def get_next_rer_A
 	ratp_page = Nokogiri::HTML(open('http://www.ratp.fr/horaires/fr/ratp/rer/prochains_passages/RA/Nation/A'))
 	
 	horaires = "************************\n"
-	ratp_page.css('fieldset.rer/table/tbody/tr').reverse[0...2].each do |l|
-		tds = l.css("td").map { |td| td.text.strip }
+	ratp_page.css('fieldset.rer/table/tbody/tr').reverse[0...2].each do |tr|
+		tds = tr.css("td").map { |td| td.text.strip }
 		horaires << "#{tds[0]} -> #{tds[1]}: #{tds[2]}\n               -----\n"	
 	end
 	horaires
